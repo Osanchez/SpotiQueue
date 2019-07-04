8f1e39c40af3634f82b4067a6a0a6887a9e0c3e5
Omar Sanchez <osanchez@umass.edu>
2019-07-04 16:29:03 -0400

    login and register page

--- SpotiQueue/urls.py
+++ SpotiQueue/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from SpotiQueue import views
 from users import views as user_views
 
@@ -22,6 +23,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
     path('authorize/', views.authorize, name='authorize'),
-    path('login/', views.authorize, name='login'),
-    path('register/', user_views.register, name='register')
+    path('register/', user_views.register, name='register'),
+    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
 ]
